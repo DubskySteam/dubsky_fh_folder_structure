5731eab1ba1b7b56c1a0e0896a4eec3d0fe7655d
Dubsky <master@dubskysarchiv.de>
2020-10-13 02:08:05 +0200

    Final

---
+++ start.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import os, sys
+
+#>> Diesen Pfad anpassen
+path = 'c:/users/dubsky/desktop/'
+#>> Anzahl der Ordner
+targetx = 14
+
+#Ordnername
+folder_name_main = 'Uni Stuff'
+#Unterordnername
+folder_name_sub1 = 'Mathe-Vorkurs'
+#Unterordnername 2
+folder_name_sub2 = 'Tag '
+#Unterstruktur
+folder_name_struc = ['Anaconda', 'PDF']
+
+var_struc = len(folder_name_struc)
+print(var_struc)
